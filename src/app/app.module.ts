d9cd5356072c4d9bae4bbe5bd48dcea1ef19ada0
L-A-M-J-O <lamjo30@gmail.com>
2022-12-12 15:22:39 +0100

    creando items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { CardComponent } from './shared/components/card/card.component';
+import { ItemsComponent } from './shared/components/items/items.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    CardComponent
+    CardComponent,
+    ItemsComponent
   ],
   imports: [
     BrowserModule
