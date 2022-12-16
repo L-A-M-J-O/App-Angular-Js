5b0d5a5cf5c0f1b7b7259d2b99a728e37c3d0d45
L-A-M-J-O <lamjo30@gmail.com>
2022-12-16 23:33:10 +0100

    Practicando

--- src/app/modules/listas/listas-basicas/listas-basicas.component.ts
+++ src/app/modules/listas/listas-basicas/listas-basicas.component.ts
@@ -1,14 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
+export type Producto = {
+  name:string;
+  price:number;
+  description:string;
+}
+
 @Component({
   selector: 'app-listas-basicas',
   templateUrl: './listas-basicas.component.html',
   styleUrls: ['./listas-basicas.component.css']
 })
+
 export class ListasBasicasComponent implements OnInit {
 
-  listaDeElementos:string[] = ['Leche', 'Ternera', 'Verdura', 'Huevos'];
+  listaDeElementos:Producto[] = [
+    {
+      name: 'Leche',
+      price: 20,
+      description: 'Vaca'
+    },
+    {
+      name: 'Verduras',
+      price: 10,
+      description: 'Tierra'
+    },
+    {
+      name: 'Carne',
+      price: 30,
+      description: 'Animales'
+    },
+  ];
 
+  opcion:number = 0;
   cargando:boolean = false;
 
   constructor() { }
@@ -16,9 +40,11 @@ export class ListasBasicasComponent implements OnInit {
   ngOnInit(): void {
 
   }
-
   cambiarCargando () {
     this.cargando = !this.cargando;
+  }
+  escogerOpcion(opcionEscogida:number){
+    this.opcion = opcionEscogida;
   }
 
 }
